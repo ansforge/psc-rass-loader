0f114acd14afa395e9e41857f6cec518eba15436
pam199968 <patrick.mortas.ext@asipsante.fr>
2021-12-08 22:29:45 +0100

    Serialization of the registry

--- pscload/src/main/java/fr/ans/psc/pscload/component/ProcessRegistry.java
+++ pscload/src/main/java/fr/ans/psc/pscload/component/ProcessRegistry.java
@@ -1,16 +1,64 @@
 package fr.ans.psc.pscload.component;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
 import fr.ans.psc.pscload.service.LoadProcess;
 
 @Component
-public class ProcessRegistry {
+public class ProcessRegistry implements Serializable {
+	
+
+	private static final long serialVersionUID = -8371349854297886378L;
+
+    @Value("${files.directory}")
+    private String filesDirectory;
 	
 	private Map<String, LoadProcess> registry = new HashMap<String, LoadProcess>();
 	
+	private int id;
+	
+	public int nextId() {
+		return ++id;
+	}
+	
+	public int currentId() {
+		return id;
+	}
+	
+	/**
+	 * Call this method in the shutdownHook to save the state of the process
+	 * @throws FileNotFoundException
+	 */
+    public void serialize() throws FileNotFoundException {
+    	Kryo kryo = new Kryo();
+    	kryo.register(getClass());
+        Output output = new Output(new FileOutputStream(filesDirectory + File.separator + "registry.ser"));
+        kryo.writeClassAndObject(output, this);
+        output.close();
+    }
+	
+    public void deserialize() throws FileNotFoundException {
+    	Kryo kryo = new Kryo();
+    	kryo.register(getClass());
+        Input input = new Input(new FileInputStream(filesDirectory + File.separator + "registry.ser"));
+        kryo.readClassAndObject(input);
+        input.close();
+    }
+    
+    
 	public void register(String id, LoadProcess process) {
 		registry.put(id, process);
 	}
@@ -26,4 +74,5 @@ public class ProcessRegistry {
 	public boolean isEmpty() {
 		return registry.isEmpty();
 	}
+	
 }
--- pscload/src/main/java/fr/ans/psc/pscload/state/ProcessState.java
+++ pscload/src/main/java/fr/ans/psc/pscload/state/ProcessState.java
@@ -1,12 +1,13 @@
 package fr.ans.psc.pscload.state;
 
+import java.io.Serializable;
+
 import fr.ans.psc.pscload.service.LoadProcess;
 import fr.ans.psc.pscload.state.exception.LoadProcessException;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-public abstract class ProcessState {
+public abstract class ProcessState implements Serializable {
 	
+	private static final long serialVersionUID = -7973864026513440841L;
 	protected LoadProcess process;
 	
 	abstract public void runTask() throws LoadProcessException;
