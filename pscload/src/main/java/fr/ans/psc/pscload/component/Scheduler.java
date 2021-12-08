99b5b7beb8d56e89a65fca3a0d2258d7b22fcabd
pmortas <patrick.mortas.ext@esante.gouv.fr>
2021-12-08 17:16:24 +0100

    Add metrics for external process check

--- pscload/src/main/java/fr/ans/psc/pscload/component/Scheduler.java
+++ pscload/src/main/java/fr/ans/psc/pscload/component/Scheduler.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import fr.ans.psc.pscload.metrics.CustomMetrics;
 import fr.ans.psc.pscload.service.LoadProcess;
 import fr.ans.psc.pscload.state.FileDownloaded;
 import fr.ans.psc.pscload.state.FileExtracted;
@@ -22,6 +23,9 @@ public class Scheduler {
 
     @Autowired
     private ProcessRegistry processRegistry;
+    
+    @Autowired
+    private CustomMetrics customMetrics;
 
     @Value("${enable.scheduler:true}")
     private boolean enabled;
@@ -37,7 +41,7 @@ public class Scheduler {
 
     @Value("${ca.path}")
     private String cafile;
-
+    
     /**
      * Download and parse.
      */
@@ -50,9 +54,11 @@ public class Scheduler {
             	// On lance le premier step
 				process.runtask();
 				process.setState(new FileDownloaded());
+				customMetrics.getAppGauges().get(CustomMetrics.CustomMetric.STAGE).set(10);
 				// Step Deux : Extraction
 				process.runtask();
 				process.setState(new FileExtracted());
+				customMetrics.getAppGauges().get(CustomMetrics.CustomMetric.STAGE).set(20);
 			} catch (LoadProcessException e) {
 				// TODO log
 				processRegistry.unregister(process.toString());
