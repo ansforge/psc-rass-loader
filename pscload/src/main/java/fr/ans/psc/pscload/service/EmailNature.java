daaef7cb1de5736b2533de4411a8887324814e79
pmortas <patrick.mortas.ext@esante.gouv.fr>
2021-12-14 16:58:07 +0100

    add rabbitmq and email service

---
+++ pscload/src/main/java/fr/ans/psc/pscload/service/EmailNature.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright A.N.S 2021
+ */
+package fr.ans.psc.pscload.service;
+
+/**
+ * The Enum EmailNature.
+ */
+public enum EmailNature {
+    
+    /** The process finished. */
+    PROCESS_FINISHED("Le process pscload s'est terminé, le fichier %s a été généré à partir du fichier %s.", "PSCLOAD - Fin de process"),
+    
+    /** The process relaunched. */
+    PROCESS_RELAUNCHED("Le fichier %s n'est pas cohérent avec le fichier %s. Reprise du process.", "PSCLOAD - Reprise du process");
+
+    /** The message. */
+    public String message;
+    
+    /** The subject. */
+    public String subject;
+
+    /**
+     * Instantiates a new email nature.
+     *
+     * @param message the message
+     * @param subject the subject
+     */
+    EmailNature(String message, String subject) {this.message = message; this.subject = subject;}
+}
---
+++ pscload/src/main/java/fr/ans/psc/pscload/service/EmailService.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright A.N.S 2021
+ */
+package fr.ans.psc.pscload.service;
+
+import java.io.File;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+/**
+ * The Class EmailService.
+ */
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    @Value("${pscload.mail.receiver}")
+    private String receiver;
+
+    /**
+     * Send mail.
+     *
+     * @param emailNature the email nature
+     * @param latestTxtAndSer the latest txt and ser
+     */
+    public void sendMail(EmailNature emailNature, Map<String, File> latestTxtAndSer) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(sender);
+        String[] allReceivers = receiver.split(",");
+        message.setTo(allReceivers);
+        message.setSubject(emailNature.subject);
+        message.setText(getEmailMessage(emailNature, latestTxtAndSer));
+
+        emailSender.send(message);
+    }
+
+    private String getEmailMessage(EmailNature emailNature, Map<String, File> latestTxtAndSer) {
+        String latestTxt = latestTxtAndSer.get("txt").getName();
+        String latestSer = latestTxtAndSer.get("ser").getName();
+
+        return String.format(emailNature.message, latestSer, latestTxt);
+    }
+}
