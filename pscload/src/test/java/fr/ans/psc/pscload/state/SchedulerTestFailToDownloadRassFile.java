1726057d7cf0b9bd7889b38ddbe5c187e9de0bcd
pmortas <patrick.mortas.ext@esante.gouv.fr>
2021-12-18 16:41:23 +0100

    New test

---
+++ pscload/src/test/java/fr/ans/psc/pscload/state/SchedulerTestFailToDownloadRassFile.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright A.N.S 2021
+ */
+package fr.ans.psc.pscload.state;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.delete;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.put;
+import static com.github.tomakehurst.wiremock.client.WireMock.any;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+
+import fr.ans.psc.pscload.PscloadApplication;
+import fr.ans.psc.pscload.component.ProcessRegistry;
+import fr.ans.psc.pscload.component.Scheduler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * The Class DiffComputedStateTest.
+ */
+@Slf4j
+@SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = PscloadApplication.class)
+@AutoConfigureMockMvc
+public class SchedulerTestFailToDownloadRassFile {
+
+	@Autowired
+	private ProcessRegistry registry;
+
+	@Autowired
+	private Scheduler scheduler;
+	
+	@Autowired
+	private MockMvc mockmvc;
+
+	/** The http  mock server. */
+	@RegisterExtension
+	static WireMockExtension httpMockServer = WireMockExtension.newInstance()
+			.options(wireMockConfig()
+					.dynamicPort()
+					.usingFilesUnderClasspath("wiremock"))
+			.build();
+
+	@DynamicPropertySource
+	static void registerPgProperties(DynamicPropertyRegistry propertiesRegistry) {
+		propertiesRegistry.add("extract.download.url",
+				() -> httpMockServer.baseUrl() + "/V300/services/extraction/Extraction_ProSanteConnect");
+		propertiesRegistry.add("files.directory",
+				() -> Thread.currentThread().getContextClassLoader().getResource("work").getPath());
+		propertiesRegistry.add("api.base.url", () -> httpMockServer.baseUrl());
+		propertiesRegistry.add("use.x509.auth", () -> "false");
+		propertiesRegistry.add("enable.scheduler", () -> "true");
+	}
+
+	@BeforeEach
+	void setup() {
+		registry.clear();
+		// clear work directory
+		File outputfolder = new File(Thread.currentThread().getContextClassLoader().getResource("work").getPath());
+		File[] files = outputfolder.listFiles();
+		if (files != null) { // some JVMs return null for empty dirs
+			for (File f : files) {
+				f.delete();
+			}
+		}
+	}
+
+	@Test
+	@DisplayName("Scheduler Error Download test (bad url)")
+	void schedulerTest() throws Exception {
+		// Configure the mock service to serve zipfile
+		String contextPath = "/V300/services/extraction/Extraction_ProSanteConnectFalse";
+		httpMockServer.stubFor(get(contextPath).willReturn(aResponse().withStatus(404)));
+		scheduler.run();
+		assertTrue(registry.isEmpty());
+	}
+
+}
