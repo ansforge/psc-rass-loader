13af99264ab2505ad2bae5349b96209b1694d1b2
bbonche <bbonche@henix.fr>
2021-12-28 15:09:20 +0100

    add ChangesApplied resumption case

--- pscload/src/main/java/fr/ans/psc/pscload/PscloadApplication.java
+++ pscload/src/main/java/fr/ans/psc/pscload/PscloadApplication.java
@@ -51,6 +51,10 @@ public class PscloadApplication {
 
 	@Value("${files.directory:.}")
 	private String filesDirectory;
+
+	@Value("${extract.base.url}")
+	private String extractBaseUrl;
+
 	/**
 	 * The main method.
 	 *
@@ -98,13 +102,16 @@ public class PscloadApplication {
 				LoadProcess process = registry.getCurrentProcess();
 				if (process != null) {
 					Class<? extends ProcessState> stateClass = process.getState().getClass();
-					if (stateClass.equals(UploadingChanges.class)) {
+					if (stateClass.equals(UploadingChanges.class) || stateClass.equals(ChangesApplied.class)) {
 							ForkJoinPool.commonPool().submit(() -> {
 								try {
-									// upload changes
-									customMetrics.getAppMiscGauges().get(CustomMetrics.MiscCustomMetric.STAGE).set(60);
-									process.nextStep();
-									process.setState(new ChangesApplied());
+									if (stateClass.equals(UploadingChanges.class)) {
+										// upload changes
+										customMetrics.getAppMiscGauges().get(CustomMetrics.MiscCustomMetric.STAGE).set(60);
+										process.nextStep();
+										process.setState(new ChangesApplied(customMetrics, extractBaseUrl));
+										process.getState().setProcess(process);
+									}
 									customMetrics.getAppMiscGauges().get(CustomMetrics.MiscCustomMetric.STAGE).set(70);
 									// Step 5 : call pscload
 									process.nextStep();
