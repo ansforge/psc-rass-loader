6878b0f32a34d1a4781d5001d64c67d97d2b9105
pam199968 <88666492+pam199968@users.noreply.github.com>
2021-12-30 16:51:25 +0100

    Update ProcessController.java

--- pscload/src/main/java/fr/ans/psc/pscload/controller/ProcessController.java
+++ pscload/src/main/java/fr/ans/psc/pscload/controller/ProcessController.java
@@ -54,15 +54,6 @@ public class ProcessController {
 
     private final ProcessRegistry registry;
 
-    @Value("${api.base.url}")
-    private String apiBaseUrl;
-
-    @Value("${deactivated.excluded.profession.codes:}")
-    private String[] excludedProfessions;
-
-    @Value("${pscextract.base.url}")
-    private String pscextractBaseUrl;
-
       /**
      * Instantiates a new process controller.
      *
@@ -76,7 +67,7 @@ public class ProcessController {
 	/**
 	 * Continue process.
 	 *
-	 * @return the deferred result
+	 * @return the  result
 	 */
 	@PostMapping(value = "/process/continue")
 	public ResponseEntity<Void> continueProcess() {
