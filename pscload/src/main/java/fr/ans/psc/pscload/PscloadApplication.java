5aaefe4ef7ffce9ee761687adef2d22704ae0af7
bbonche <bbonche@henix.fr>
2022-03-17 11:39:08 +0100

    use static kryo in main class rather than autowired bean because we want to be sure to access it during shutown

--- pscload/src/main/java/fr/ans/psc/pscload/PscloadApplication.java
+++ pscload/src/main/java/fr/ans/psc/pscload/PscloadApplication.java
@@ -14,11 +14,10 @@ import fr.ans.psc.pscload.metrics.CustomMetrics;
 import fr.ans.psc.pscload.model.EmailTemplate;
 import fr.ans.psc.pscload.model.LoadProcess;
 import fr.ans.psc.pscload.model.Stage;
+import fr.ans.psc.pscload.model.entities.*;
+import fr.ans.psc.pscload.model.operations.*;
 import fr.ans.psc.pscload.service.EmailService;
-import fr.ans.psc.pscload.state.ChangesApplied;
-import fr.ans.psc.pscload.state.ProcessState;
-import fr.ans.psc.pscload.state.UploadInterrupted;
-import fr.ans.psc.pscload.state.UploadingChanges;
+import fr.ans.psc.pscload.state.*;
 import fr.ans.psc.pscload.state.exception.LoadProcessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +34,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
@@ -62,9 +64,6 @@ public class PscloadApplication {
     @Autowired
     private EmailService emailService;
 
-    @Autowired
-    private Kryo kryo;
-
     @Value("${files.directory:.}")
     private String filesDirectory;
 
@@ -79,6 +78,41 @@ public class PscloadApplication {
 
     @Value("${api.base.url}")
     private String apiBaseUrl;
+
+    private static Kryo kryo;
+
+    static {
+        kryo = new Kryo();
+        OperationMapSerializer operationMapSerializer = new OperationMapSerializer();
+        kryo.register(HashMap.class, 9);
+        kryo.register(ArrayList.class, 10);
+        kryo.register(Professionnel.class, 11);
+        kryo.register(ExerciceProfessionnel.class, 12);
+        kryo.register(SavoirFaire.class, 13);
+        kryo.register(SituationExercice.class, 14);
+        kryo.register(RefStructure.class, 15);
+        kryo.register(Structure.class, 16);
+        kryo.register(ProcessRegistry.class, 17);
+        kryo.register(LoadProcess.class, 18);
+        kryo.register(ProcessState.class, 19);
+        kryo.register(Submitted.class, 20);
+        kryo.register(DiffComputed.class, 21);
+        kryo.register(ReadyToComputeDiff.class, 22);
+        kryo.register(ReadyToExtract.class, 23);
+        kryo.register(UploadingChanges.class, 24);
+        kryo.register(ChangesApplied.class, 25);
+        kryo.register(String[].class, 27);
+        kryo.register(ConcurrentHashMap.class, 28);
+        kryo.register(UploadInterrupted.class, 29);
+        kryo.register(SerializationInterrupted.class, 30);
+        kryo.register(OperationMap.class, operationMapSerializer, 31);
+        kryo.register(PsCreateMap.class, 32);
+        kryo.register(PsUpdateMap.class, operationMapSerializer, 33);
+        kryo.register(PsDeleteMap.class, 34);
+        kryo.register(StructureCreateMap.class, 35);
+        kryo.register(StructureUpdateMap.class, operationMapSerializer, 36);
+        kryo.register(StructureDeleteMap.class, 37);
+    }
 
     /**
      * The main method.
