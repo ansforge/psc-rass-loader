f6925c0f8f24027a8f40fc3da0713912e2275a15
ssviridov.henix <ssviridov@henix.fr>
2022-06-29 17:04:54 +0200

    added FirstName static methods from the api to Prenom

--- pscload/src/main/java/fr/ans/psc/pscload/model/entities/Prenom.java
+++ pscload/src/main/java/fr/ans/psc/pscload/model/entities/Prenom.java
@@ -6,31 +6,68 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Prenom extends fr.ans.psc.model.FirstName {
-
+  private static final int FIRST_NAME_COUNT = 3;
   String firstName;
   Integer order;
 
-    public Prenom() {
-    }
+  public Prenom() {
+  }
 
-    public Prenom(String firstName, Integer order) {
-        this.firstName = firstName;
-        this.order = order;
-    }
+  public Prenom(String firstName, Integer order) {
+    this.firstName = firstName;
+    this.order = order;
+  }
 
-    public String getFirstName() {
-        return firstName;
-    }
+  public Prenom(String firstName) {
+    this.firstName = firstName;
+    this.order = 0;
+  }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public Integer getOrder() {
-        return order;
-    }
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-    public void setOrder(Integer order) {
-        this.order = order;
+  public Integer getOrder() {
+    return order;
+  }
+
+  public void setOrder(Integer order) {
+    this.order = order;
+  }
+
+  /**
+   * Transforms a string of first names separated by an apostrophe (')
+   * into a list of FirstName objects, in the same order as they appear in the string.
+   * @param string a string of first names separated by an apostrophe (') and containing exactly FIRST_NAME_COUNT-1 apostrophes (')
+   * @return list of FirstName objects
+   */
+  public static List<FirstName> stringToList(String string) {
+    String[] firstNameStrings = string.split("'");
+    List<FirstName> firstNames = new ArrayList<>();
+    for (int i = 0; i < firstNameStrings.length; i++) {
+      firstNames.add(new FirstName(firstNameStrings[i], i));
     }
+    return firstNames;
+  }
+
+  /**
+   * Transforms a list of FirstName objects into a string of first names separated by an apostrophe ('),
+   * and containing exactly FIRST_NAME_COUNT-1 apostrophes (') even if the list contains less than FIRST_NAME_COUNT names.
+   * @param firstNames list of FirstName objects
+   * @return a string of first names separated by an apostrophe (') and containing exactly FIRST_NAME_COUNT-1 apostrophes (')
+   */
+  public static String listToString(List<FirstName> firstNames) {
+    StringBuilder stringBuilder = new StringBuilder();
+
+    for (int i = 0; i < FIRST_NAME_COUNT; i++) {
+      if (i < firstNames.size()) stringBuilder.append(firstNames.get(i).getFirstName());
+      stringBuilder.append("'");
+    }
+    stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+    return stringBuilder.toString();
+  }
 }
