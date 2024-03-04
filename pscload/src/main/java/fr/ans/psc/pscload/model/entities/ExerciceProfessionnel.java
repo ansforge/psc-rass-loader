b80bcd25cf4f0ed417aa926203d57b50e6a885aa
Eric Degenetais <edegenetais@henix.fr>
2024-03-04 20:47:43 +0100

    2024 year bump.

--- pscload/src/main/java/fr/ans/psc/pscload/model/entities/ExerciceProfessionnel.java
+++ pscload/src/main/java/fr/ans/psc/pscload/model/entities/ExerciceProfessionnel.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2022-2023 Agence du Numérique en Santé (ANS) (https://esante.gouv.fr)
+ * Copyright (C) 2022-2024 Agence du Numérique en Santé (ANS) (https://esante.gouv.fr)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
