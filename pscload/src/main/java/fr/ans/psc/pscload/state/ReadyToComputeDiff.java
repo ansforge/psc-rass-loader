2d336ec61c7686d4bc9d93e31fbebf0994456cf3
Boris <boris.gazel@activus-group.fr>
2025-08-11 18:18:51 +0200

    [evol-production][US-06] Check if RPPS

--- pscload/src/main/java/fr/ans/psc/pscload/state/ReadyToComputeDiff.java
+++ pscload/src/main/java/fr/ans/psc/pscload/state/ReadyToComputeDiff.java
@@ -270,7 +270,7 @@ public class ReadyToComputeDiff extends ProcessState {
                     	}else {
 							log.debug("PS {} will not be created by RASS because it is in merged PS {}",
 									entry.getValue().getNationalId(), psInDb.getNationalId());
-							if(psInRass.getProfessions() != null && !psInRass.getProfessions().equals(psInDb.getProfessions())) {
+							if("RPPS".equals(psInRass.getOrigin()) && psInRass.getProfessions() != null && !psInRass.getProfessions().equals(psInDb.getProfessions())) {
 								log.debug("Different professions, update will be performed");
 								OperationMap<String, RassEntity> mapUpdate = process.getMaps().stream()
 									    .filter(m -> OperationType.UPDATE.equals(m.getOperation()))
