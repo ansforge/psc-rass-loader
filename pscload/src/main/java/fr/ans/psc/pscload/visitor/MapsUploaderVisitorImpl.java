b1c966fcba04653f26dd8e54e56f0671c694961e
GaultierEviden <gaultier.moraillon@eviden.com>
2026-02-05 11:17:22 +0100

    check exerciceProfessionnel is null in MapsUploaderVisitorImpl.java

--- pscload/src/main/java/fr/ans/psc/pscload/visitor/MapsUploaderVisitorImpl.java
+++ pscload/src/main/java/fr/ans/psc/pscload/visitor/MapsUploaderVisitorImpl.java
@@ -128,7 +128,7 @@ public class MapsUploaderVisitorImpl implements MapsVisitor {
 				List<Profession> psExPros = prof.getProfessions();
 				AtomicBoolean deletable = new AtomicBoolean(true);
 				psExPros.forEach(exerciceProfessionnel -> {
-					if (excludedProfessions != null && Arrays.stream(excludedProfessions)
+					if (exerciceProfessionnel != null && excludedProfessions != null && Arrays.stream(excludedProfessions)
 							.anyMatch(profession -> exerciceProfessionnel.getCode().equals(profession))) {
 						deletable.set(false);
 					}
